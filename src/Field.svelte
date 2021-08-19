a1944492107ddcd7959d21a82552515269048c85
Alfred Ringstad <alfred.ringstad@hyperlab.se>
2021-08-19 11:11:28 +0200

    Fix issue when item is not found

--- src/Field.svelte
+++ src/Field.svelte
@@ -40,34 +40,36 @@
   {:then items}
     <div class="items">
       {#each items as item}
-        <div class="card">
-          {#if item.image}
-            <img
-              src={`${item.image}?extend=copy&width=128&method=crop&height=128&quality=100`}
-              alt="Summary"
-            />
-          {/if}
-          <div class="details">
-            <span>{item.type}</span>
-            <h3>{item.title}</h3>
-            <div class="controls">
-              <a href={item.url} target="_blank">View on site</a>
-              <button on:click={() => deselect(item.id)}
-                ><svg
-                  height="20"
-                  width="20"
-                  viewBox="0 0 20 20"
-                  aria-hidden="true"
-                  focusable="false"
-                  class="css-19bqh2r"
-                  ><path
-                    d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"
-                  /></svg
-                ></button
-              >
+        {#if item}
+          <div class="card">
+            {#if item.image}
+              <img
+                src={`${item.image}?extend=copy&width=128&method=crop&height=128&quality=100`}
+                alt="Summary"
+              />
+            {/if}
+            <div class="details">
+              <span>{item.type}</span>
+              <h3>{item.title}</h3>
+              <div class="controls">
+                <a href={item.url} target="_blank">View on site</a>
+                <button on:click={() => deselect(item.id)}
+                  ><svg
+                    height="20"
+                    width="20"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                    focusable="false"
+                    class="css-19bqh2r"
+                    ><path
+                      d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"
+                    /></svg
+                  ></button
+                >
+              </div>
             </div>
           </div>
-        </div>
+        {/if}
       {/each}
     </div>
   {/await}
