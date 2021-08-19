7c8445d2f78ab2892c1b2be10250aa09e5d1753d
Alfred Ringstad <alfred.ringstad@hyperlab.se>
2021-08-19 11:17:29 +0200

    Add possibility to remove unknown items

--- src/Field.svelte
+++ src/Field.svelte
@@ -37,9 +37,9 @@
 {#if promise}
   {#await promise}
     <p>Loading...</p>
-  {:then items}
+  {:then fetchedItems}
     <div class="items">
-      {#each items as item}
+      {#each fetchedItems as item, index}
         {#if item}
           <div class="card">
             {#if item.image}
@@ -66,6 +66,28 @@
                     /></svg
                   ></button
                 >
+              </div>
+            </div>
+          </div>
+        {:else}
+          <div class="card">
+            <div class="details">
+              <span>Unknown</span>
+              <h3>{$items[index].type}:{$items[index].id}</h3>
+              <div class="controls">
+                <button on:click={() => deselect($items[index].id)}>
+                  <svg
+                    height="20"
+                    width="20"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                    focusable="false"
+                    class="css-19bqh2r"
+                    ><path
+                      d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"
+                    /></svg
+                  >
+                </button>
               </div>
             </div>
           </div>
